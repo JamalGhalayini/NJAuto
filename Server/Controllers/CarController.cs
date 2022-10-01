ea10b662dc506543137c85c17a473d5bcfe7e282
Nikolaos Chanania <nich32832@edu.ucl.dk>
2022-10-01 17:57:40 +0200

    mud blazor

---
+++ Server/Controllers/CarController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NJAuto.Shared.Models;
+
+namespace NJAuto.Server.Controllers
+{
+    [ApiController]
+    [Route("/carcontroller")]
+    public class CarController 
+    {
+        [HttpGet]
+        public IEnumerable<Car> GetCars()
+        { 
+            List<Car> cars = new List<Car>();
+            cars.Add(new Car
+            {
+                CarId = 1,
+                NumberPlat = "CV1232",
+                Imag="Cars/golf.jpg",
+                Brand = "WV Golf",
+                year = 2007,
+                Km = 200000,
+                Price = 40000
+            });
+            return cars;
+        }
+    }
+}
