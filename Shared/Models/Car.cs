65ab0b0b7fb0f9ab0204156b9d761b718409b934
Jamal Abed El-Naser Ghalayini <jaeg32833@edu.ucl.dk>
2022-11-08 12:29:38 +0100

    FixingCarDeatils

--- Shared/Models/Car.cs
+++ Shared/Models/Car.cs
@@ -9,10 +9,9 @@ namespace NJAuto.Shared.Models
     public class Car
     {
         public int CarId { get; set; }
-        public string NumberPlat { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
-        public string Imag { get; set;  }
+        public string Image { get; set;  }
         public double Km { get; set; }
         public int Year { get; set; }
         public double Price { get; set; }
