4714f53b34364787b765debb055a7ace9bf2e306
Jamal Abed El-Naser Ghalayini <jaeg32833@edu.ucl.dk>
2022-11-15 13:36:02 +0100

    OpretServiceClassAndInterface

---
+++ Server/Controllers/Services/CarService.cs
@@ -0,0 +1,27 @@
+﻿using NJAuto.Shared.Models;
+
+namespace NJAuto.Server.Controllers.Services
+{
+    public class CarService : ICarServices
+    {
+        public Task AddCar(Car newCar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditCar(Car Car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Car>> GetCar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveCar(Car Car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Server/Controllers/Services/ICarServices.cs
@@ -0,0 +1,12 @@
+﻿using NJAuto.Shared.Models;
+
+namespace NJAuto.Server.Controllers.Services
+{
+    public interface ICarServices
+    {
+        public Task AddCar(Car newCar);
+        public Task RemoveCar(Car Car);  
+        public Task EditCar(Car Car);
+        public Task<IEnumerable<Car>> GetCar();
+    }
+}
