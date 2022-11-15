de09a5bb7947a90d1155ae707353d5d3306339f7
Jamal Abed El-Naser Ghalayini <jaeg32833@edu.ucl.dk>
2022-11-15 12:47:17 +0100

    AddingEntityFremwork

---
+++ Server/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿namespace NJAuto.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Shared.Models.Car> Car { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutinfAssembly);
+        //}
+
+
+
+    }
+}
--- Shared/Models/Car.cs
+++ Shared/Models/Car.cs
@@ -11,7 +11,8 @@ namespace NJAuto.Shared.Models
         public int CarId { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
-        public string Image { get; set;  }
+        public string ImageTitle { get; set;  }
+        public byte[] ImageData { get; set; }
         public double Km { get; set; }
         public int Year { get; set; }
         public double Price { get; set; }
